f844455947b7aeed46f86bf219eaa028f148411c
Megntong Chen <mc6@illinois.edu>
2018-12-10 20:09:51 -0600

    a small container demo

--- catastrophic_container.c
+++ catastrophic_container.c
@@ -1,3 +1,24 @@
 /**
  * CS 296 MP catastrophic container
 */
+
+
+#include "catastrophic_container.h"
+
+
+int child(void *args){
+    printf("pid as seen in the child: %d\n", getpid());
+    system("bash");
+}
+
+
+int main(){
+    int namespace = CLONE_NEWUTS|CLONE_NEWPID|CLONE_NEWIPC|CLONE_NEWNS|CLONE_NEWNET;
+    pid_t p = clone(child, malloc(4096)+4096, SIGCHLD|namespace, NULL);
+    if(p == -1){
+        perror_die("clone");
+    }
+    printf("child pid: %d./na   \n", p);
+    waitpid(p, NULL, 0);
+    return 0;
+}
--- catastrophic_container.h
+++ catastrophic_container.h
@@ -3,3 +3,19 @@
 */
 
 #pragma once
+
+#define _GNU_SOURCE 
+#include <sched.h>
+
+#include <stdio.h>
+#include <stdlib.h>
+#include <signal.h>
+
+#include <sys/types.h>
+#include <sys/wait.h>
+#include <unistd.h>
+
+#include "includes/utils.h"
+
+
+int child(void *args);
